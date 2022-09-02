8a25c1afadc0e10fe20466e2cc717084806bb242
Karena Stevens <kaymark09@gmail.com>
2022-09-01 23:28:16 -0400

    created db & established connection

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,23 @@
+from ssl import ALERT_DESCRIPTION_UNKNOWN_PSK_IDENTITY
 from app import app
+import mysql.connector
+
+import os
 
 from flask import (Blueprint, render_template)
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
+my_conn=mysql.connector.connect(
+    user = os.environ.get("DB_USER"),
+    password = os.environ.get("DB_PASS"),
+    db = os.environ.get("DB_NAME"),
+    host = os.environ.get("DB_HOST"),
+)
+
 @bp.route("/")
 def main():
-    return render_template('main.html')
+    mycursor = my_conn.cursor()
+    mycursor.execute("SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime")
+    rows = mycursor.fetchall()
+    return render_template('main.html', rows=rows)
