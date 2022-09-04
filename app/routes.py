4563a0f187e9a797b008c06e59eb49d1dcbe9007
Karena Stevens <kaymark09@gmail.com>
2022-09-04 07:52:51 -0400

    add daily method to show day's appts

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,8 @@ from app import app
 import mysql.connector
 from app.forms import AppointmentForm
 import os
-from flask import (Blueprint, render_template, redirect)
-from datetime import datetime
+from flask import (Blueprint, render_template, redirect, url_for)
+from datetime import (datetime, timedelta)
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -16,15 +16,22 @@ my_conn=mysql.connector.connect(
 )
 
 
-@bp.route("/", methods=['GET', 'POST'])
+@bp.route("/")
 def main():
+    d = datetime.now()
+    return redirect(url_for(".daily", year=d.year, month=d.month, day=d.day))
+
+@bp.route('/<int:year>/<int:month>/<int:day>', methods=['GET', 'POST'])
+def daily(year, month, day):
     mycursor = my_conn.cursor()
     form = AppointmentForm()
-    data = ()
     if form.validate_on_submit():
         mycursor.execute("INSERT INTO appointments(name, start_datetime, end_datetime, description, private) VALUES(%s, %s, %s, %s, %s)", (form.name.data, datetime.combine(form.start_date.data, form.start_time.data), datetime.combine(form.end_date.data, form.end_time.data), form.description.data, form.private.data))
         my_conn.commit()
         return redirect('/')
-    mycursor.execute("SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime")
+
+    day = datetime(year, month, day)
+    next_day = (day + timedelta(days=1))
+    mycursor.execute("SELECT id, name, start_datetime, end_datetime FROM appointments WHERE start_datetime BETWEEN %s AND %s ORDER BY start_datetime", (day, next_day))
     rows = mycursor.fetchall()
     return render_template('main.html', rows=rows, form=form)
